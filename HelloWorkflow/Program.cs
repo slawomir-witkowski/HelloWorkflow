fece061b766e879683f0b29a1a92343c1e5e0556
slawomir-witkowski <switkows@gmail.com>
2020-08-30 16:11:53 +0200

    Initial commit

---
+++ HelloWorkflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorkflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
